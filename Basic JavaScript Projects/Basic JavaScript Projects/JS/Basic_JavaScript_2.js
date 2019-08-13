991b78b618873a7f86077196125b42003e4ff06d
haiwilson <52790398+haiwilson@users.noreply.github.com>
2019-08-13 11:47:52 -0400

    Push of my “Basic JavaScript Projects” folder to JavaScript Projects GitHub repository

---
+++ Basic JavaScript Projects/Basic JavaScript Projects/JS/Basic_JavaScript_2.js
@@ -0,0 +1,11 @@
+// JavaScript source code
+function My_First_Function() { 
+var String = "This text is green!";  
+var result = String.fontcolor("green"); 
+document.getElementById ("Green_Text").innerHTML = result;
+}
+function myFunction() {
+	var sentence = "I am learning";
+	sentence += "a lot from this book!";
+	document.getElementById("concatenate").innerHTML = sentence;
+}
---
+++ Basic JavaScript Projects/Basic JavaScript Projects/JS/Basic_JavaScript_3.js
@@ -0,0 +1,12 @@
+function addition Function() { 
+	var addition = 2 +2;
+	document.getElementById("Math").innerHTML = "2 + 2 =" + addition;
+}
+function multiplication Function() { 
+	var simple_math = 2 * 2;
+	document.getElementById("Math").innerHTML = "2 * 2 =" + simple_math;
+}
+function division Function() { 
+	var simple_math = 2 / 2;
+	document.getElementById("Math").innerHTML = "2 / 2 =" + simple_math;
+}
