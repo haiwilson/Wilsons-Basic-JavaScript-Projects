13777c7822d7607dc21a22fc1366b2e05c46e76a
haiwilson <52790398+haiwilson@users.noreply.github.com>
2019-08-19 10:18:26 -0400

    Push of HTML version 7 to github repository

---
+++ Basic_JavaScript_7.js
@@ -0,0 +1,14 @@
+function Time_function() { 
+	var Time = new Date().getHours();
+	var Reply;
+	if (Time < 12 == Time > 0) {
+		Reply = "Its morning time";
+	}
+	else if (Time > 12 == Time < 10) {
+		Reply = "Its afternoon time";
+	}
+	else {
+		Reply = "Its evening time";
+	}
+	document.getElementById("Time_of_day").innerHTML = Reply;
+}
