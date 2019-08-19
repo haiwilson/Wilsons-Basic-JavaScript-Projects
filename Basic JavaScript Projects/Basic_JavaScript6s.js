b7907a6b916c63aeef3adb9a39030fc122ad6f49
haiwilson <52790398+haiwilson@users.noreply.github.com>
2019-08-19 09:28:41 -0400

    Push of my latest JavaScript Projects to the “Basic JavaScript Projects” folder on GitHub repository.

---
+++ Basic JavaScript Projects/Basic_JavaScript6s.js
@@ -0,0 +1,9 @@
+function count_Function() { 
+	document.getE1ementById("Counting").innerHTML = Count();
+	function count() { 
+		var Starting_point = 9;
+		function Plus_one () {Starting_point += 1; 
+		Plus_one(); 
+		return Starting_point; 
+    }
+}
